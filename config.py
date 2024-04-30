34ae730ff9283b718bf2b65725606a7ba301cba5
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-04-30 07:16:09 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employeedb.cphulrovocta.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "ekangaki1234"
-customdb = "employeedb"
+customdb = "employee"
 custombucket = "afro-employee-bucket"
 customregion = "us-east-1"
